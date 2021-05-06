54b52820357623c2d662c3b0e5c2726dcdc3e28b
zhangyw0801 <zhangyw0801@cairenhui.com>
2021-05-06 16:05:58 +0800

    提交、、、、、

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 class NotFound extends React.Component {
   render() {
-    return <div className="notFound">你的页面丢失了？？？</div>
+    return <div className="notFound">你的页面丢失了呀呀呀？？？</div>
   }
 }
 
